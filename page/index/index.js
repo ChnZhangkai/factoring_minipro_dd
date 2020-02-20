2ce4bd9b2fe2a513e03b26ec6b93cba61396d229
zhangkai <649959626@qq.com>
2020-02-20 16:16:51 +0800

    精简项目

--- page/index/index.js
+++ page/index/index.js
@@ -1,5 +1,12 @@
 Page({
-    data:{
-        
+    data: {
+        h5url: null
+    },
+    onLoad() {
+        var that = this
+        that.setData({
+            // h5url: 'https://wwww.baidu.com/'
+            h5url: 'http://localhost:8080/#/'
+        })
     }
 })
